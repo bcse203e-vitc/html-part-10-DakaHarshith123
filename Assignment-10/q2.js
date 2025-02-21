72b3e8b13d268e7a16f39017fa4757c8987033a3
DakaHarshith123 <daka.harshith2023@vitstudent.ac.in>
2025-02-21 12:57:56 +0530

    Create q2.js

---
+++ Assignment-10/q2.js
@@ -0,0 +1,9 @@
+document.getElementById("redButton").addEventListener("click", function()
+{
+    document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("blueButton").addEventListener("click", function()
+{
+    document.body.style.backgroundColor = "blue";
+});
